7ea11f7d6ae0b062c2cf12fad501fb4b786eec21
Emiliopg91 <ojosdeserbio@gmail.com>
2024-11-19 01:21:23 +0100

    latest-linux

--- py_modules/utils/appimage_tool.py
+++ py_modules/utils/appimage_tool.py
@@ -1,11 +1,15 @@
 from .github_helper import GithubHelper
 from .desktop_parser import DesktopParser
+from datetime import datetime
 
+import base64
+import hashlib
 import os
 import re
 import shutil
 import subprocess
 import tempfile
+import yaml
 
 class AppImageTool:
     def __init__(self, github_helper: GithubHelper) -> None:
@@ -57,7 +61,7 @@ class AppImageTool:
         os.chdir(self.tmp_path)
 
         file_name = re.sub(r"[^a-zA-Z0-9]", "-", name)
-        appimage_path = os.path.join(self.working_dir, f"{file_name}-{version}.AppImage")
+        appimage_path = os.path.join(self.working_dir, f"{file_name}.AppImage")
         print(f"Generating AppImage file '{file_name}'")
         command = (
             f'ARCH=x86_64 {self.appimagetool_path} --comp gzip {self.tmp_path} "{appimage_path}" '
@@ -72,10 +76,48 @@ class AppImageTool:
             raise RuntimeError(f"Command finished with exit code {result.returncode}")
 
         shutil.move(os.path.join(self.tmp_path, f"{os.path.basename(appimage_path)}.zsync"), f"{appimage_path}.zsync")
-
         self.github_helper.set_github_env_variable("APPIMAGE_PATH", appimage_path)
+        
+        latest_linux_path = os.path.join(self.working_dir, "latest-linux.yml")
+        print("Generating latest-linux.yml")
+
+        sha512 = self.get_sha512(appimage_path)
+
+        data={}
+        data["version"] = version
+        data["files"] = {}
+        data["files"]["url"] = file_name + ".AppImage"
+        data["files"]["sha512"] = sha512
+        data["files"]["size"] = os.path.getsize(appimage_path)
+        data["files"]["blockMapSize"] = data["files"]["size"]/1024
+        data["path"] = file_name + ".AppImage"
+        data["sha512"] = sha512
+        data["releaseDate"] = self.get_release_date(appimage_path)
+                
+        with open(latest_linux_path, "w") as file:
+                yaml.dump(data, file, default_flow_style=False, sort_keys=False)
+        self.github_helper.set_github_env_variable("LATEST_LINUX_PATH", latest_linux_path)
 
         os.chdir(prev_cwd)
+
+    def get_release_date(self, path):
+        try:
+            timestamp = os.stat(path).st_birthtime
+        except AttributeError:
+            timestamp = os.stat(path).st_mtime
+
+        dt = datetime.utcfromtimestamp(timestamp)
+        return dt.strftime('%Y-%m-%dT%H:%M:%S.%f')[:-3] + 'Z'
+
+    def get_sha512(self, path):
+        sha512 = hashlib.sha512()
+
+        with open(path, "rb") as archivo:
+            for bloque in iter(lambda: archivo.read(8192), b""):
+                sha512.update(bloque)
+        
+        hash_base64 = base64.b64encode(sha512.digest()).decode('utf-8')
+        return hash_base64
 
     def cleanup(self):
         print("Cleaning workspace and temporal files")
